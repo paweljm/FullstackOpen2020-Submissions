5f643c98312b39f2a672d1e7b0b6c81d7f24f03b
Pawel Marcinkiewicz <paweljmarcinkiewicz@gmail.com>
2023-05-02 23:00:25 +0100

    2.5: separate module

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,28 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => 
+  <>
+    {parts.map(part => <Part key={part.id} part={part} />)} 
+  </>
+
+const Course = ({course}) => {
+  const total = course.parts.reduce((total, part) => total + part.exercises, 0)
+  console.log(total)
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total sum={total} />
+    </>
+    
+  )
+}
+
+export default Course
