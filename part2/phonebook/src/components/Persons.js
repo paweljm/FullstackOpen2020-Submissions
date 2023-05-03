18b53ab543e5de9e0673d74cafb3b9a68af9510c
Pawel Marcinkiewicz <paweljmarcinkiewicz@gmail.com>
2023-05-03 23:14:01 +0100

    2.11: The Phonebook Step6

--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,3 +1,3 @@
-const Persons = ({persons, search}) => persons.filter(person => ((person.name).toLowerCase()).includes(search)).map(person => <p key={person.name}>{person.name} {person.number}</p>)
+const Persons = ({persons, search}) => persons?.filter(person => ((person.name).toLowerCase()).includes(search)).map(person => <p key={person.name}>{person.name} {person.number}</p>)
 
 export default Persons
